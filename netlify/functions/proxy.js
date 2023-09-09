318e0549c5c6d8453b3e3b50aafac2db2694c9c1
Jonah Koppel <Jonahkoppelv2@gmail.com>
2023-09-09 01:56:05 -0400

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,6 +1,43 @@
+const https = require('https');
+
 export async function handler(event, context) {
-    return {
-      statusCode: 200,
-      body: "Hello, World!"
-    };
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
   }
+
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+  const API_KEY = process.env.OPENAI_API_KEY;
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${API_KEY}`
+      }
+    }, res => {
+      let data = '';
+
+      res.on('data', chunk => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        resolve({
+          statusCode: 200,
+          body: data
+        });
+      });
+    });
+
+    req.on('error', error => {
+      resolve({
+        statusCode: 500,
+        body: JSON.stringify({error: error.message})
+      });
+    });
+
+    req.write(event.body);
+    req.end();
+  });
+}
