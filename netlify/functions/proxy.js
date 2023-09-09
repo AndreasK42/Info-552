5484cd024810459dfd44ae57e844b8d912061494
Jonah Koppel <Jonahkoppelv2@gmail.com>
2023-09-09 00:15:59 -0400

    hopefully created serverless backend for api key

---
+++ netlify/functions/proxy.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+  const API_KEY = process.env.OPENAI_API_KEY;
+
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${API_KEY}`
+    },
+    body: event.body
+  });
+
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+}
